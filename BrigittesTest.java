1fdd4444c0700f72a7a86c3417433d0b624c0876
Stefan-24 <124808745+Stefan-24@users.noreply.github.com>
2023-02-08 18:53:51 -0500

    Update BrigittesTest.java

--- BrigittesTest.java
+++ BrigittesTest.java
@@ -9,6 +9,8 @@ public class BrigittesTest {
         System.out.println("Hello Grade 11s");
         System.out.println("This is a test from Brigitte"); // 02/3/23 8:50am
         System.out.println("Collaborate below!!");
+        
+        System.out.println("This is Sam's line, I'll change it when the story starts");
     }
 }
 
